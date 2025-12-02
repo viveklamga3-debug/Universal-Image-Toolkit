ff3cb0d7beb2f9380112d7f376dd43e4e9291f5f
Vivek lamga <viveklamga3@gmail.com>
2025-12-02 08:33:06 +0000

    I want only first 4 features and remove rest of the features

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -3,11 +3,6 @@ import {
   Maximize,
   Scissors,
   FileImage,
-  Code,
-  Palette,
-  Tags,
-  Wand2,
-  RotateCw,
   type LucideIcon,
 } from 'lucide-react';
 
@@ -42,35 +37,5 @@ export const TOOLS: Tool[] = [
     description: 'Crop to a specific area',
     icon: Crop,
     href: '/tools/image-cropper',
-  },
-  {
-    name: 'Image to Base64',
-    description: 'Embed images in code',
-    icon: Code,
-    href: '/tools/base64-converter',
-  },
-  {
-    name: 'Color Picker',
-    description: 'Extract colors from images',
-    icon: Palette,
-    href: '/tools/color-picker',
-  },
-  {
-    name: 'Metadata Viewer',
-    description: 'View basic image info',
-    icon: Tags,
-    href: '/tools/metadata-viewer',
-  },
-  {
-    name: 'Background Remover',
-    description: 'Remove image backgrounds',
-    icon: Wand2,
-    href: '/tools/background-remover',
-  },
-  {
-    name: 'Image Rotator / Flipper',
-    description: 'Rotate and flip images',
-    icon: RotateCw,
-    href: '/tools/image-rotator',
   },
 ];
